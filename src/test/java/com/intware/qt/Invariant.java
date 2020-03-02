86cc9c9aa580e36cdabc6dcd54211892149a8ac6
mr-rampage <readonly@che.wbac.ca>
2020-03-02 17:02:05 -0500

    feat - adding sample invariant test

---
+++ src/test/java/com/intware/qt/Invariant.java
@@ -0,0 +1,23 @@
+package com.intware.qt;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class Invariant {
+
+    @Test
+    void test_list_size_should_remain_constant_after_concatenation() {
+        qt().forAll(
+                lists().of(strings().allPossible().ofLengthBetween(1, 100)).ofSizes(integers().from(0).upTo(50)),
+                lists().of(strings().allPossible().ofLengthBetween(1, 100)).ofSizes(integers().from(0).upTo(50))
+            ).check((a, b) -> Stream.concat(a.stream(), b.stream()).count() == a.size() + b.size());
+    }
+}
