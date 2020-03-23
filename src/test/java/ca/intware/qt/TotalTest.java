3b659de80ed7f5a6c18f47fb65c672dcea2f1750
mr-rampage <readonly@che.wbac.ca>
2020-03-22 23:31:36 -0400

    refactor - simplifying generator example

--- src/test/java/ca/intware/qt/TotalTest.java
+++ src/test/java/ca/intware/qt/TotalTest.java
@@ -7,21 +7,20 @@ import org.junit.jupiter.api.Test;
 
 import javax.money.CurrencyUnit;
 
+import static ca.intware.qt.generators.CartDSL.carts;
 import static java.util.Locale.CANADA;
 import static javax.money.Monetary.getCurrency;
 import static org.quicktheories.QuickTheory.qt;
-import static org.quicktheories.generators.SourceDSL.integers;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class TotalTest {
 
-    private Cart cart;
     private final CurrencyUnit CAD = getCurrency(CANADA);
 
     @Test
     void should_calculate_the_total_of_the_shopping_cart() {
         qt()
-                .forAll(integers().all())
-                .check(generatedInteger -> cart.total().equals(Money.zero(CAD)));
+                .forAll(carts())
+                .check(cart -> cart.total().equals(Money.zero(CAD)));
     }
 }
--- src/test/java/ca/intware/qt/generators/CartDSL.java
+++ src/test/java/ca/intware/qt/generators/CartDSL.java
@@ -1,14 +1,32 @@
 package ca.intware.qt.generators;
 
 import ca.intware.qt.Cart;
+import ca.intware.qt.Product;
+import ca.intware.qt.ProductPriceCatalog;
 import org.quicktheories.core.Gen;
+
+import java.util.List;
+
+import static ca.intware.qt.generators.ProductPriceCatalogDSL.productPriceCatalogs;
 
 public final class CartDSL {
     public static Gen<Cart> carts() {
-        throw new UnsupportedOperationException("You complete me...");
+        return productPriceCatalogs()
+                .flatMap(catalog -> purchaseList(catalog).map(purchases -> {
+                    var cart = new Cart(catalog);
+                    purchases.forEach(cart::add);
+                    return cart;
+                }));
     }
 
-    private static Gen<Cart> emptyCarts() {
-        throw new UnsupportedOperationException("You had me at 'hello'...");
+
+    /**
+     * Creates a list of random products from a product price catalog
+     *
+     * @param productPriceCatalog product price catalog
+     * @return A Gen of List of Product
+     */
+    private static Gen<List<Product>> purchaseList(ProductPriceCatalog productPriceCatalog) {
+        throw new UnsupportedOperationException("You complete me...");
     }
 }
